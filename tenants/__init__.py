dcc8154dea7f734ebc542daddb6de2047c486d00
mohammad alqudah <mohammad.y.qudah@gmail.com>
2025-11-16 11:59:03 +0300

    feat: add multitenancy support with django-tenants configuration

---
+++ tenants/__init__.py
@@ -0,0 +1,3 @@
+default_app_config = "tenants.apps.TenantsConfig"
+
+
---
+++ tenants/apps.py
@@ -0,0 +1,13 @@
+from django.apps import AppConfig
+
+
+class TenantsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "tenants"
+
+from django.apps import AppConfig
+
+
+class TenantsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "tenants"
